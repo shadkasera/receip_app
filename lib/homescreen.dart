871aae7c92c7b0161927325129ce9954faed8540
Shadreck kasera <shadkasera@gmail.com>
2023-04-06 01:06:34 +0200

    updated homescreen file

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -7,61 +7,135 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset(
-                  'assets/images/chips.jpg',
-                  width: 100,
-                  height: 100,
-                ),
-                Image.asset(
-                  'assets/images/chicken.jpeg',
-                  width: 100,
-                  height: 100,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset(
-                  'assets/images/noodles1.png',
-                  width: 100,
-                  height: 100,
-                ),
-                Image.asset(
-                  'assets/images/pizza1.png',
-                  width: 100,
-                  height: 100,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Image.asset(
-                  'assets/images/cake.jpeg',
-                  width: 100,
-                  height: 100,
-                ),
-                Image.asset(
-                  'assets/images/beef1.png',
-                  width: 100,
-                  height: 100,
-                ),
-              ],
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/images/chips.jpg',
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image.asset(
+                    'assets/images/chicken.jpeg',
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/images/noodles1.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image.asset(
+                    'assets/images/pizza1.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/images/cake.jpeg',
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image.asset(
+                    'assets/images/beef1.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+               const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/images/cake.jpeg',
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image.asset(
+                    'assets/images/beef1.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+               const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/images/cake.jpeg',
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image.asset(
+                    'assets/images/beef1.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+               const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/images/cake.jpeg',
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image.asset(
+                    'assets/images/beef1.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+               const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/images/cake.jpeg',
+                    width: 100,
+                    height: 100,
+                  ),
+                  Image.asset(
+                    'assets/images/beef1.png',
+                    width: 100,
+                    height: 100,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
