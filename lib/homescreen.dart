42f0de8616ae9d5fccce68c1ce2fa8779c5bec14
Shadreck kasera <shadkasera@gmail.com>
2023-04-24 10:59:52 +0200

    updated dart files

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -20,63 +20,185 @@ class HomeScreen extends StatelessWidget {
                   Text('Welcome Amos Kuyenda'),
                 ],
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: const [
                     Icon(Icons.arrow_back),
                     Icon(Icons.arrow_forward),
-              
                   ],
                 ),
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: const [
-                   Text('Receipe list',style: TextStyle(fontSize: 18),),
-                   Text('View all',style: TextStyle(fontSize: 18),),
-                 
-              
+                    Text(
+                      'Receipe list',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      'View all',
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ],
                 ),
               ),
-              const SizedBox(height: 10,),
+              const SizedBox(
+                height: 10,
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children:  [
-                   Padding(
-                     padding: const EdgeInsets.symmetric(horizontal: 20),
-                     child: Column(
-                       children:  [
-                         const Padding(
-                           padding: EdgeInsets.symmetric(vertical: 10),
-                           child: Text('Cake',style: TextStyle(fontSize: 18),),
-                         ),
-                         Image.asset('assets/images/cake.jpeg',height: 70,),
-                       ],
-                     ),
-                   ),
-                  Padding(
-                     padding: const EdgeInsets.symmetric(horizontal: 20),
-                     child: Column(
-                       children:  [
-                         const Padding(
-                           padding: EdgeInsets.symmetric(vertical: 10),
-                           child: Text('Pizza',style: TextStyle(fontSize: 18),),
-                         ),
-                         Image.asset('assets/images/pizza1.png',height: 70,),
-                       ],
-                     ),
-                   ),
-                 
-              
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              'Cake',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Image.asset(
+                            'assets/images/cake.jpeg',
+                            height: 70,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              'Pizza',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Image.asset(
+                            'assets/images/pizza1.png',
+                            height: 70,
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
+              //another
+               
+              //another
+               const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              'Noodles',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Image.asset(
+                            'assets/images/noodles1.png',
+                            height: 70,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              'Chips',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Image.asset(
+                            'assets/images/chips.jpg',
+                            height: 70,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //another
+               const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              'Beef',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Image.asset(
+                            'assets/images/beef1.png',
+                            height: 70,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              'Chicken',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                          Image.asset(
+                            'assets/images/chicken.jpeg',
+                            height: 70,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              //another
+              
             ],
           ),
         ),
